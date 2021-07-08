387fd6f7aba147bd58639e459d650d4f77704b39
Charles Pinheiro <charles_fagundes@hotmail.com>
2021-07-07 22:37:33 -0300

    feat/Componente Header pronto, esperando o componente MenuDashboard para integração

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { Container, Title } from "../Header/styles";
+
+const Header = () => {
+
+   return (
+      <Container>
+         <Title>MesaCheia</Title>
+         <div>Menu Dashboard</div>
+      </Container>
+   );
+};
+
+export default Header;
