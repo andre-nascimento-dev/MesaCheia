2db50a382ea0ef80e11e676d31931b99b28e0f57
Guilherme Gonçalves <gcgoncalves13@gmail.com>
2021-07-08 14:26:29 +0000

    feature: Table Card structure

---
+++ src/components/TableCard/index.tsx
@@ -0,0 +1,39 @@
+import { Table } from "../../types";
+import Button from "../Button";
+import { Container, Counter, Content } from "./styles";
+
+interface TableCardProps {
+  table: Table;
+  isJoin: boolean;
+  onClick: () => void;
+}
+
+const TableCard = ({ table, isJoin, onClick }: TableCardProps) => {
+  return (
+    <Container>
+      <div>
+        <h2>{table.name}</h2>
+        <Counter>
+          {table.players.length}/{table.total}
+        </Counter>
+      </div>
+
+      <div>
+        <Content>
+          <p>
+            <span>Tema:</span> {table.theme}
+          </p>
+          <p>
+            <span>Sistema:</span> {table.system}
+          </p>
+          <p>
+            <span>{table.userId}</span>
+          </p>
+        </Content>
+        <Button onClick={onClick}>{isJoin ? "Ver Mais" : "Ver Mais"}</Button>
+      </div>
+    </Container>
+  );
+};
+
+export default TableCard;
