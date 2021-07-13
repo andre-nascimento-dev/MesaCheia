5a8ffa03180142cc6b4233b7f06448de21b565f6
Andre Nascimento <andredossantosnascimento1510@gmail.com>
2021-07-13 07:11:35 -0300

    feat/login estilizado

--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -1,13 +1,44 @@
 import styled from "styled-components";
-
+import loginBg from "../../assets/img/login-background.jpg";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  height: 100vh;
   width: 100vw;
-  color: var(--secondaryTextColor);
+  max-width: 1400px;
+  padding: 0 12px 24px;
+  margin: 0 auto;
 `;
+
 export const BoxContent = styled.div`
-  background-image: url(../assets/img/login-background.jpg);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 1400px;
+  padding: 0 12px 24px;
+  flex-grow: 1;
+
+  @media (min-width: 768px) {
+    height: 80vh;
+    background: url(${loginBg}) no-repeat center;
+    background-size: cover;
+  }
+`;
+export const Redirect = styled.span`
+  font-size: 0.7rem;
+  color: #fff;
+  padding-top: 16px;
+
+  a {
+    color: #fff;
+    font-weight: bold;
+    transition: color 300ms ease-in-out;
+
+    &:hover {
+      color: #000;
+    }
+  }
 `;
